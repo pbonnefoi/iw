ff481da3f0fd5e6606658730015c56ca6d86e672
Pierre BONNEFOI <pierre.bonnefoi@gmail.com>
2014-08-19 18:47:54 +0200

    commit of the day

--- sites/all/themes/iconic/templates/node/node--award.tpl.php
+++ sites/all/themes/iconic/templates/node/node--award.tpl.php
@@ -10,7 +10,11 @@
 <article class="box post node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <header>
     <?php if ($title): ?>
-      <span class="trophy icon fa-trophy <?php print '' ?>"><?php print render($content['field_date_award']); ?></span>
+      <div class="trophy">
+        <?php print render($content['field_date_award']); ?>
+        <span class="icon fa-trophy"></span>
+        <span class="icon fa-anchor <?php print '' ?>"></span>
+      </div>
       <h2><a href="<?php print $node_url; ?>"><?php print t('The') ?> <?php print $title; ?> - <?php print render($content['field_watch_reference']); ?> <?php print render($content['field_date_award']); ?> <?php print render($content['field_award_category']); ?> <?php print variable_get('site_name', t('Iconic Watch.')) . ' ' . t('award'); ?></a></h2>
     <?php endif; ?>
 
