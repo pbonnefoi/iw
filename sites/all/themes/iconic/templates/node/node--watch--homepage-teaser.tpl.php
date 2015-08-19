16b4637fe18ebe52623e50ea833e1ecd2013e7f3
Pierre BONNEFOI <p.bonnefoi@mazarinedigital.com>
2015-08-19 18:41:18 +0200

    intg new templates

--- sites/all/themes/iconic/templates/node/node--watch--homepage-teaser.tpl.php
+++ sites/all/themes/iconic/templates/node/node--watch--homepage-teaser.tpl.php
@@ -7,43 +7,27 @@
  * @see https://drupal.org/node/1728164
  */
 ?>
-<article class="box post node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <header>
-    <?php if ($title): ?>
-      <div class="image logo">
-        <a href="<?php print url('taxonomy/term/' . $brand_tid); ?>"><?php print render($brand_logo); ?></a>
-      </div>
-      <h3<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h3>
-      <?php if ($display_submitted): ?>
-        <span class="date desktop-only"><?php print format_date($node->changed, 'short'); ?></span>
-      <?php endif; ?>
-    <?php endif; ?>
-
-    <?php if ($unpublished): ?>
-      <mark class="unpublished"><?php print t('Unpublished'); ?></mark>
-    <?php endif; ?>
-  </header>
-  <div class="watch-attributes-wrapper watch-attributes">
-    <span class="image featured image-watch"><?php print render($content['field_watch_picture']); ?></span>
-    <ul class="attributes icons">
-      <li class="icon fa-tag"><span><?php print t('Reference') ?> : </span><?php print render($content['field_watch_reference']); ?></li>
-      <li class="icon fa-cog"><span><?php print t('Caliber') ?> : </span><?php print render($content['field_caliber']); ?></li>
-      <li class="icon fa-arrows-h"><span><?php print t('Diameter') ?> : </span><?php print render($content['field_diameter']); ?></li>
-      <?php if (isset($content['field_water_resistant']) && $content['field_water_resistant']): ?>
-        <li class="icon fa-tint"><span><?php print t('Water Resistant') ?> : </span><?php print render($content['field_water_resistant']); ?></li>
-      <?php endif ?>
-      <li class="icon fa-calendar"><span><?php print t('Production Date') ?> : </span><?php print render($content['field_production_year']); ?></li>
-      <?php if (isset($content['field_price_new']) && $content['field_price_new']): ?>
-        <li class="icon fa-money"><span><?php print t('Price new') ?> : </span><?php print render($content['field_price_new']); ?>€</li>
-      <?php endif ?>
-      <?php if (isset($content['field_price_used_low']) && $content['field_price_used_low']): ?>
-        <li class="icon fa-money"><span><?php print t('Price used') ?> : </span><?php print render($content['field_price_used_low']); ?>€ / <?php print render($content['field_price_used_high']); ?>€</li>
-      <?php endif ?>
-    </ul>
-    <div>
-      <?php if (isset($content['field_watch_description']) && $content['field_watch_description']): ?>
-        <?php print render($content['field_watch_description']); ?>
-      <?php endif; ?>
+<div class="grid-picture<?php print $class; ?>">
+  <a href="<?php print url('node/' . $node->nid); ?>" class="image featured image-watch">
+    <?php print render($content['field_watch_picture']); ?>
+  </a>
+</div>
+<div class="grid-roll-wrapper">
+  <div class="grid-roll<?php print $class; ?>">
+    <div class="brand">
+      <a href="<?php print url('taxonomy/term/' . $brand_tid); ?>">
+        <?php print render($brand_logo); ?>
+      </a>
     </div>
+    <div class="captions">
+      <a href="<?php print url('node/' . $node->nid); ?>">
+        <h3 class="grid-title"><?php print $title; ?></h3>
+      </a>
+    </div>
+    <span class="grid-learn-more">
+      <a href="<?php print url('node/' . $node->nid); ?>">
+        <span><?php print t('Learn more'); ?></span>
+      </a>
+    </span>
   </div>
-</article>
+</div>
