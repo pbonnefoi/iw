3151dd746e99a82a86483a0534f65136af887cb3
Pierre BONNEFOI <p.bonnefoi@mazarinedigital.com>
2015-08-20 18:36:01 +0200

    add some CSS

--- sites/all/themes/iconic/templates/node/node--article--homepage-teaser.tpl.php
+++ sites/all/themes/iconic/templates/node/node--article--homepage-teaser.tpl.php
@@ -10,26 +10,16 @@
 <!-- Post -->
 <article class="box post node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <header>
-    <?php if ($title): ?>
-      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>" titl="<?php print $title; ?>"><?php print $title; ?></a></h2>
-    <?php endif; ?>
-
-    <?php if ($unpublished): ?>
-      <mark class="unpublished"><?php print t('Unpublished'); ?></mark>
-    <?php endif; ?>
-  </header>
-
   <div class="image featured">
     <a href="<?php print url('node/' . $node->nid); ?>" title="<?php print $title; ?>">
       <?php print render($content['field_image']); ?>
     </a>
   </div>
 
-  <?php print render($content['body']); ?>
-
-  <ul class="actions">
-    <li><a href="<?php print url('node/' . $node->nid); ?>" class="button icon fa-file-text"><?php print t('Learn more'); ?></a></li>
-  </ul>
+  <div class="node-article-title">
+    <?php if ($title): ?>
+      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>" titl="<?php print $title; ?>"><?php print $title; ?></a></h2>
+    <?php endif; ?>
+  </div>
 
 </article>
